441a3d82266c24727af53df4e9992b135ee11133
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-04-07 16:51:49 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/main/java/com/example/ecsv1sp/EcsV1SpApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecsv1sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsV1SpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsV1SpApplication.class, args);
+	}
+
+}
